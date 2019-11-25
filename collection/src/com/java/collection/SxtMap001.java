f44c30e80d44878ae0df3ac5f39e9c1751ab400a
Bing <power1355332693@outlook.com>
2019-11-25 16:04:56 +0800

    no message

---
+++ collection/src/com/java/collection/SxtMap001.java
@@ -0,0 +1,10 @@
+package com.java.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SxtMap001 {
+    public static void main(String[] args) {
+        Map map = new HashMap();
+    }
+} 
--- collection/src/com/java/collection/TestMap.java
+++ collection/src/com/java/collection/TestMap.java
@@ -9,6 +9,7 @@ public class TestMap {
         map.put("老刘", new Wife("小丽"));
         map.put("老李", new Wife("小铭"));
         Wife w = (Wife) map.get("老李");
+        map.containsKey("l老刘");
         System.out.println(w);
     }
 }
